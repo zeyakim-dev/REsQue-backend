e5aaca0720347dd994b67c4729e6f1203ce76249
zeyakim-dev <zeyakimdev@gmail.com>
2024-09-21 23:28:52 +0900

    ✨ SoftDeletionMixin 구현
    - Entity 의 sof 삭제를 위한 믹스인 구현

---
+++ app/shared/domain/mixin/soft_deletion_mixin.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+@dataclass
+class SoftDeletionMixin:
+    is_deleted: bool = False
+    deleted_at: Optional[datetime] = None
