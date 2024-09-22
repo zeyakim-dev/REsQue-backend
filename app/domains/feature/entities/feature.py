afb7b347e240b3ef3bb1bcb9d8ba66a3f4460074
zeyakim-dev <zeyakimdev@gmail.com>
2024-09-22 13:50:26 +0900

    ✨ Feature 도메인

---
+++ app/domains/feature/entities/feature.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+from app.shared.domain.base_entity import BaseEntity
+from app.shared.domain.mixin.soft_deletion_mixin import SoftDeletionMixin
+from app.shared.domain.mixin.timestamp_mixin import TimestampMixin
+
+@dataclass(kw_only=True)
+class Feature(BaseEntity, TimestampMixin, SoftDeletionMixin):
+    domain_id: int
+    name: str
+    description: str
