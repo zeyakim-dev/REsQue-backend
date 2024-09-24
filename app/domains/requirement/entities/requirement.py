2a3602270812490990b25af5deeadcca23e391c0
zeyakim-dev <zeyakimdev@gmail.com>
2024-09-24 11:17:29 +0900

    🎨 entity의 업데이트 기능 추가

--- app/domains/requirement/entities/requirement.py
+++ app/domains/requirement/entities/requirement.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, replace
 from datetime import datetime
 from typing import Optional
 
@@ -12,7 +12,7 @@ from app.shared.domain.base_entity import BaseEntity
 from app.shared.domain.mixin.soft_deletion_mixin import SoftDeletionMixin
 from app.shared.domain.mixin.timestamp_mixin import TimestampMixin
 
-@dataclass(kw_only=True)
+@dataclass(frozen=True, kw_only=True)
 class Requirement(BaseEntity, TimestampMixin, SoftDeletionMixin):
     feature_id: int
     author_id: int
@@ -24,3 +24,21 @@ class Requirement(BaseEntity, TimestampMixin, SoftDeletionMixin):
     complexity: RequirementComplexity
     assignee_id: Optional[int] = None
     expected_completion_date: Optional[datetime] = None
+
+    def update(self, updating_entity: "Requirement") -> "Requirement":
+        updatable_attrs = [
+            "feature_id", "title", "description", "type", "status", 
+            "priority", "complexity", "assignee_id", "expected_completion_date"
+        ]
+        
+        update_dict = {
+            attr: getattr(updating_entity, attr)
+            for attr in updatable_attrs
+            if getattr(updating_entity, attr) != getattr(self, attr)
+        }
+        
+        if update_dict:
+            update_dict["updated_at"] = datetime.now()
+            return replace(self, **update_dict)
+        
+        return self
