b032f165e8d5c684ce53b08a41f5c62ba12c132f
zeyakim-dev <zeyakimdev@gmail.com>
2024-09-24 10:55:04 +0900

    ✨ SQLAlchemy에서 사용할 모델구현과 기본형 더미 추가

---
+++ app/domains/requirement/infrastructure/database/models/sql/sql_requirment.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class SQLRequirement(Base):
+    __tablename__ = "requirements"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    status = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
